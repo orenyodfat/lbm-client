ae10be3055499da65a84a93440088f3ace2b4b09
gilse <gil@b-buzzy.com>
2014-08-18 15:16:20 +0300

    scanning qr

--- src/com/lazooz/lbm/MainZoozActivity.java
+++ src/com/lazooz/lbm/MainZoozActivity.java
@@ -1,8 +1,13 @@
 package com.lazooz.lbm;
 
+
 import com.lazooz.lbm.utils.BBUncaughtExceptionHandler;
 
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainZoozActivity extends ActionBarActivity {
@@ -13,13 +18,33 @@ public class MainZoozActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		Thread.setDefaultUncaughtExceptionHandler( new BBUncaughtExceptionHandler(this));
 		setContentView(R.layout.activity_main_zooz);
+		
+		
+		Button scanBtn = (Button)findViewById(R.id.zooz_scan_btn);
+		scanBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(MainZoozActivity.this, CameraQRActivity.class);
+				startActivityForResult(intent, 1);
+			}
+		});
 
 		
 		
 	}
 	
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == 1) {
+			String s = intent.getStringExtra("DATA");
+			Toast.makeText(this, s,  Toast.LENGTH_LONG).show();
+		}
+		
 
+          
+	}
 
 
 }
