599ca8ffebd289a7eb8793ce945d18cdd338708a
oren <orenyodfat>
2014-10-29 08:43:46 +0200

    Sound disable by default

--- src/com/lazooz/lbm/preference/SettingsActivity.java
+++ src/com/lazooz/lbm/preference/SettingsActivity.java
@@ -22,7 +22,7 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
 	
 	public static final boolean PREF_MINING_CHARGER_CONNECTIVITY_DEFAULT = true;
 	public static final boolean PREF_MINING_MINING_ENAB_DIS_DEFAULT = true;
-	public static final boolean PREF_SOUND_GESTERS_ENAB_DIS_DEFAULT = true;
+	public static final boolean PREF_SOUND_GESTERS_ENAB_DIS_DEFAULT = false;
 	
 	
 	
